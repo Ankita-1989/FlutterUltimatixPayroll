bd0cf1ad3f3188f457abec0a60a2a97244499686
Ankita-1989 <47313111+Ankita-1989@users.noreply.github.com>
2021-04-16 16:33:07 +0530

    h

--- lib/api_repository/api.dart
+++ lib/api_repository/api.dart
@@ -6,21 +6,7 @@ import 'package:flutter_payrollapp/Constant/Constant.dart';
 import 'package:flutter_payrollapp/Constant/PreferenceUtils.dart';
 import 'package:http/http.dart' as http;
 
-Future callPostMethod(BuildContext context, String url, Map params) async {
-  var body=json.encode(params);
-  return await http.post(url, body: body, headers: {
-    "Content-Type": "application/json",
-    HttpHeaders.authorizationHeader:"Bearer "+PreferenceUtils.getString("Bearer")
-  }).then((http.Response response) {
-    print(response.statusCode);
-    final int statusCode = response.statusCode;
-    if (statusCode < 200 || statusCode > 404 || json == null) {
-      // throw new Exception("Error while fetching data");
-      return Constant.error_response;
-    }
-    return response.body;
-  });
-}
+
 
 Future callGetMethod(BuildContext context, String url) async {
   return await http
